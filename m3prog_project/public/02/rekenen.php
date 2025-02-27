7ea9c3fc65c4b0a8ee71b792219f40a97a290051
ColinJMMC <colin.ivko@gmail.com>
2025-02-27 10:19:03 +0100

    opdracht 2

---
+++ m3prog_project/public/02/rekenen.php
@@ -0,0 +1,27 @@
+<?php 
+
+$getal = 1.23532;
+$afgerond1 = round($getal);
+$getal1 = 1.83532;
+$afgerond2 = round($getal, 0 ,PHP_ROUND_HALF_DOWN);
+
+echo "Als je $getal afrond dan krijg je: $afgerond1";
+echo "<br>";
+echo "Als je $getal1 afrond dan krijg je: $afgerond2";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+<!-- http://localhost:88/02/rekenen.php -->
