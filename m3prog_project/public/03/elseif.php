bb4a0cc7ad77e60d4d2380dc61bef4dd977c8ab9
ColinJMMC <colin.ivko@gmail.com>
2025-03-06 09:59:07 +0100

    Les 3

---
+++ m3prog_project/public/03/elseif.php
@@ -0,0 +1,14 @@
+<?php
+$stoplichtKleur = 'R';
+
+if($stoplichtKleur == 'R'){
+    echo  "<button style='background-color:#FF0000;height:25px;width:25px;'/>";
+} elseif($stoplichtKleur == 'O'){
+    echo  "<button style='background-color:#FFA500;height:25px;width:25px;'/>";
+} elseif($stoplichtKleur == 'G'){
+    echo  "<button style='background-color:#7fff00;height:25px;width:25px;'/>";
+}
+
+?>
+
+<!-- http://localhost:88/03/elseif.php -->
